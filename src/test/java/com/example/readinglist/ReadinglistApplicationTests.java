55cd230475ed71ceb062654b3992f17345300ac2
mi <bermetmatkurbanova@gmail.com>
2020-04-13 19:48:29 +0300

    Initial commit

---
+++ src/test/java/com/example/readinglist/ReadinglistApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.readinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+@WebAppConfiguration
+
+
+@SpringBootTest
+class ReadinglistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
